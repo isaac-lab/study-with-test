a50380caf863ddec42eb723d92bc9c58ce400664
Isaac Silvério <isaacjs.lab@gmail.com>
2019-11-15 00:45:14 +0000

    estrutura de pastas

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './pages/Welcome'
+
+function App() {
+  return (
+    <div className="App">
+        <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/pages/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Styles from './styles';
+
+function Welcome() {
+  return (
+    <div>
+        Olá mundo, Seja bem vindo
+    </div>
+  );
+}
+
+export default Welcome;
